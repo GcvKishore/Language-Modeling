b546117c12742fee8a16c6018940345eec9ae046
GcvKishore <gcvkishore@msitprogram.net>
2021-11-03 16:27:19 +0530

    code for graphTopNextWords()

--- language.py
+++ language.py
@@ -269,8 +269,12 @@ Parameters: 2D list of strs ; str
 Returns: None
 '''
 def graphTopNextWords(corpus, word):
-    return
-
+    uni_count=countUnigrams(corpus)
+    bi_count=countBigrams(corpus)
+    bigramProb=buildBigramProbs(uni_count,bi_count)
+    count=getTopWords(10,bigramProb[word]["words"],bigramProb[word]["probs"],ignore)
+    plot=barPlot(count,"Top Next Words")
+    return plot
 
 '''
 setupChartData(corpus1, corpus2, topWordCount)
